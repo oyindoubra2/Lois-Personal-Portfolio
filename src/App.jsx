d7694a9eb7771a4054b13bc658930510a6658e48
oyindoubra2 <loisziboh2@gmail.com>
2023-10-02 06:54:44 +0100

    New

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+function App(){
+  return(
+    <header className="header">
+      
+      <nav className="navbar">
+            <a href="##">LZ</a>
+            <ul>
+            <li><a href="##">Home</a></li>
+            <li><a href="##">About</a></li>
+            <li><a href="##">Porfolio</a></li>
+            <li><a href="##">Contact Me</a></li>
+            </ul>
+            <img src="/menu.svg" alt="menu" width={"24px"} height={"24px"}/>
+      </nav>
+      
+      <div className="hero">
+      <h1>Hi, Im <br />Front-end Developer</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Eos dolorum voluptate nobis accusantium blanditiis! Lorem ipsum dolor sit amet consectetur, 
+        adipisicing elit. Numquam dolor fuga quam natus quidem ipsam modi sapiente similique,
+        Tempore maxime dolorem illo porro repellendus voluptates.</p>
+      <a href="##"><button className="btn">Contact Me</button></a>
+    </div>
+    </header>
+
+ 
+  
+    
+    
+  )
+}
+
+export default App
